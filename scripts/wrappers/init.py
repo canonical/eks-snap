0b73cb22483b4282a91bdf2ca9487e0f8153e891
Konstantinos <kos.tsakalozos@canonical.com>
2020-12-03 18:15:07 +0000

    Seems to be working.

--- scripts/wrappers/init.py
+++ scripts/wrappers/init.py
@@ -14,8 +14,8 @@ def init():
     if click.confirm('Do you want to enable EBS support?'):
         click.echo('')
         click.echo('A user with proper IAM permissions need to be specified. See ')
-        key = click.prompt('The access key id of the authorised user: ')
-        access = click.prompt('The secret access key of the authorised user: ')
+        key = click.prompt('The access key id of the authorised user')
+        access = click.prompt('The secret access key of the authorised user')
         click.echo('Enabling EBS')
         source_dir = "{}/actions/aws-ebs-csi-driver".format(os.getenv('SNAP'))
         destination_dir = "{}/actions/aws-ebs-csi-driver".format(os.getenv('SNAP_DATA'))
@@ -23,13 +23,13 @@ def init():
         shutil.copytree(source_dir, destination_dir)
         with open("{}/secret.yaml".format(source_dir), "rt") as f:
             data = f.read()
-            data.replace('{{key_id}}', key)
-            data.replace('{{access_key}}', access)
+            data = data.replace('{{key_id}}', key)
+            data = data.replace('{{access_key}}', access)
             secrets_file = "{}/secret.yaml".format(destination_dir)
             os.chmod(secrets_file, 0o600)
             with open(secrets_file, "w+") as s:
                 s.write(data)
-        Path('{}/var/lock/ebs-ready').touch()
+        Path('{}/var/lock/ebs-ready'.format(os.getenv('SNAP_DATA'))).touch()
 
 
 if __name__ == '__main__':
