40ae0293663e06d723472c39d3d4b2cf5887abb6
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-12-04 14:15:31 +0200

    Init script update

--- scripts/wrappers/init.py
+++ scripts/wrappers/init.py
@@ -12,7 +12,7 @@ def init():
     Initialise the EKS cluster
     """
     if click.confirm('Do you want to enable EBS support?'):
-        click.echo('A user with proper IAM permissions need to be specified. See ')
+        click.echo('A user with proper IAM permissions need to be specified.')
         key = click.prompt('The access key id of the authorised user')
         access = click.prompt('The secret access key of the authorised user')
         source_dir = "{}/actions/aws-ebs-csi-driver".format(os.getenv('SNAP'))
@@ -30,6 +30,18 @@ def init():
         Path('{}/var/lock/ebs-ready'.format(os.getenv('SNAP_DATA'))).touch()
         click.echo('')
     if click.confirm('Do you want to enable EFS support?'):
+        fsid = click.prompt('The EFS file system ID')
+        size = click.prompt('The size in GB of the volume to be created')
+        source_dir = "{}/actions/aws-efs-csi-driver".format(os.getenv('SNAP'))
+        destination_dir = "{}/actions/aws-efs-csi-driver".format(os.getenv('SNAP_DATA'))
+        shutil.rmtree(destination_dir, ignore_errors=True)
+        shutil.copytree(source_dir, destination_dir)
+        with open("{}/setup/pv.yaml".format(source_dir), "rt") as f:
+            data = f.read()
+            data = data.replace('{{fsid}}', fsid)
+            data = data.replace('{{size}}', size)
+            with open("{}/setup/pv.yaml".format(destination_dir), "w+") as s:
+                s.write(data)
         Path('{}/var/lock/efs-ready'.format(os.getenv('SNAP_DATA'))).touch()
         click.echo('')
 
