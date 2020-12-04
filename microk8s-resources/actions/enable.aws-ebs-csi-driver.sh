d5e154c8ea14542d922299fe0349a1c133d1788e
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-12-04 14:07:02 +0200

    Setup storage classes

--- microk8s-resources/actions/enable.aws-ebs-csi-driver.sh
+++ microk8s-resources/actions/enable.aws-ebs-csi-driver.sh
@@ -9,4 +9,8 @@ echo "Enabling EBS driver"
 "$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -f $SNAP_DATA/actions/aws-ebs-csi-driver/secret.yaml
 sleep 3
 "$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -k $SNAP_DATA/actions/aws-ebs-csi-driver/overlays/stable/
+sleep 5
+sleep 5
+"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -f $SNAP_DATA/actions/aws-ebs-csi-driver/setup
+
 echo "EBS driver is enabled"
--- microk8s-resources/actions/enable.aws-efs-csi-driver.sh
+++ microk8s-resources/actions/enable.aws-efs-csi-driver.sh
@@ -6,5 +6,8 @@ source $SNAP/actions/common/utils.sh
 
 echo "Enabling EFS driver"
 
-"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -k $SNAP/actions/aws-efs-csi-driver/overlays/stable/
+"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -k $SNAP_DATA/actions/aws-efs-csi-driver/overlays/stable/
+sleep 5
+"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -f $SNAP_DATA/actions/aws-efs-csi-driver/setup
+
 echo "EFS driver is enabled"
