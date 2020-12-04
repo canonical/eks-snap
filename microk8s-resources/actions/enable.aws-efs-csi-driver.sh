de197c5de27514c980d7300a8fd00c9b3bfb9118
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2020-12-04 10:58:42 +0200

    Correct path to ebs manifests

--- microk8s-resources/actions/enable.aws-efs-csi-driver.sh
+++ microk8s-resources/actions/enable.aws-efs-csi-driver.sh
@@ -6,5 +6,5 @@ source $SNAP/actions/common/utils.sh
 
 echo "Enabling EFS driver"
 
-"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -k $SNAP_DATA/actions/aws-efs-csi-driver/overlays/stable/
+"$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" apply -k $SNAP/actions/aws-efs-csi-driver/overlays/stable/
 echo "EFS driver is enabled"
