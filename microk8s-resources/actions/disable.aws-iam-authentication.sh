2344284b2bced577a8b7f2f88cb1ef42f4fd4043
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2021-01-07 15:44:32 +0200

    AWS IAM controller yamls

--- microk8s-resources/actions/disable.aws-iam-authentication.sh
+++ microk8s-resources/actions/disable.aws-iam-authentication.sh
@@ -8,9 +8,8 @@ echo "Disabling AWS IAM authentication"
 CLUSTERID="$(echo $RANDOM)"
 declare -A map
 map[\$CLUSTERID]="$CLUSTERID"
-AWS_ENV=""
-map[\$AWS_ENV]="$AWS_ENV"
 use_manifest aws-iam-authentication delete "$(declare -p map)"
+use_manifest aws-iam-authentication-daemon-local delete "$(declare -p map)"
 
 echo "Configuring the API server"
 skip_opt_in_config "authentication-token-webhook-config-file" kube-apiserver
--- microk8s-resources/actions/enable.aws-iam-authentication.sh
+++ microk8s-resources/actions/enable.aws-iam-authentication.sh
@@ -11,28 +11,26 @@ chmod 777 "${SNAP_DATA}/aws-iam-athenticator"
 CLUSTERID="$(echo $RANDOM)"
 declare -A map
 map[\$CLUSTERID]="$CLUSTERID"
+use_manifest aws-iam-authentication apply "$(declare -p map)"
 
 # Always set the default region unless we are on AWS
 # TODO make default region configurable
-AWS_ENV="
-        env:
-        - name: AWS_REGION
-          value: us-east-2
-        - name: AWS_DEFAULT_REGION
-          value: us-east-2
-"
 if [ -f /sys/hypervisor/uuid ]
   then
   EC2VM=$(head -c 3 /sys/hypervisor/uuid)
   if [ "$EC2VM" == "ec2" ];
   then
     echo "EC2 node detected"
-    AWS_ENV=""
+    use_manifest aws-iam-authentication-daemon apply
+  else
+    echo "VM but not on EC2 detected"
+    use_manifest aws-iam-authentication-daemon-local apply
   fi
+else
+  echo "No VM detected"
+  use_manifest aws-iam-authentication-daemon-local apply
 fi
-map[\$AWS_ENV]="$AWS_ENV"
 
-use_manifest aws-iam-authentication apply "$(declare -p map)"
 echo "Waiting for the authenticator service to start"
 sleep 5
 "$SNAP/kubectl" "--kubeconfig=$SNAP_DATA/credentials/client.config" -n kube-system rollout status ds/aws-iam-authenticator
